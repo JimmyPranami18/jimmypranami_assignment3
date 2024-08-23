81b3f6a5284ffab7d4795c35034ccccbcd14b900
JimmyPranami18 <navinkumarjimmy.22.cse@iite.indusuni.ac.in>
2024-08-23 10:41:14 +0530

    Create q1.sol

---
+++ q1.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether, "Minimum amount to enter is 0.01 ETH");
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address ;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
